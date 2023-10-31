4c15f2661478cdad06457b712d69bb18b678c697
mouse <orian755@gmail.com>
2023-10-31 21:11:14 +0000

    create-symlinks.sh: 	Changed all tilde references to $HOME. 	Added username to be logged in symlink_log

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# Creates symlinks for all files in the folder the script is in, except for files listed in .symlink-ignore separated by newline
+echo $HOME
+# Creates symlinks for all *dot* files in the folder the script is in, except for files listed in .symlink-ignore separated by newline
 # First argument specifies where to place all the symlinks
 
 # Log the date to ~/.dotfiles/symlink_log
-echo $(date) >> ~/.dotfiles/symlink_log
+echo $USER >> $HOME/.dotfiles/symlink_log
+echo $(date) >> $HOME/.dotfiles/symlink_log
 
 # Generate list of all files in current folder (Without recursing into existing folders) starting with '.'
 filepaths=$(
@@ -14,7 +16,7 @@ for filepath in $filepaths
 do
 	# filepaths contains full paths. The regex in 'sed' makes sure we get the filename only.
 	filename=$(echo $filepath | sed -E 's%.*\/(.+)%\1%g');
-	for fileToIgnore in $(cat ~/.dotfiles/symlink-ignore)
+	for fileToIgnore in $(cat $HOME/.dotfiles/symlink-ignore)
 	do
 		if [[ 
 			$fileToIgnore == $filename
@@ -24,8 +26,8 @@ do
 		fi
 	done	
 
-	ln -s $filepath $1/$filename 2>> ~/.dotfiles/symlink_log && echo "Created symlink for $filename in path $1.";
+	ln -s $filepath $1/$filename 2>> $HOME/.dotfiles/symlink_log && echo "Created symlink for $filename in path $1.";
 done
 
 # Add newline after each write to the log:
-echo "" >> ~/.dotfiles/symlink_log
+echo "" >> $HOME/.dotfiles/symlink_log
