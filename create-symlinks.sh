3cacef8993221c3570037efffab25f156121cb7e
pickDefault <orian755@gmail.com>
2023-11-03 16:06:39 +0000

    If no option is provided, symlinks are created at $HOME. script waits for verification before creating symlinks.

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 # Creates symlinks for all *dot* files in the folder the script is in, except for files listed in .symlink-ignore separated by newline
-# First argument specifies where to place all the symlinks
+# First argument specifies where to place all the symlinks. Default is $HOME.
 
 # Log the date to ~/.dotfiles/symlink_log
 echo $USER >> $HOME/.dotfiles/symlink_log
 echo $(date) >> $HOME/.dotfiles/symlink_log
+
+option=${1:-$HOME}
+echo "Creating symlinks in $option"
+echo "continue? [y/n]"
+read -n 1 
+if [[ $REPLY != "y" ]];
+then
+	echo ""
+	exit 1
+fi
+echo ""
 
 # Generate list of all files in current folder (Without recursing into existing folders) starting with '.'
 filepaths=$(
@@ -25,7 +36,7 @@ do
 		fi
 	done	
 
-	ln -s $filepath $1/$filename 2>> $HOME/.dotfiles/symlink_log && echo "Created symlink for $filename in path $1.";
+	ln -s $filepath $option/$filename 2>> $HOME/.dotfiles/symlink_log && echo "Created symlink for $filename in path $option.";
 done
 
 # Add newline after each write to the log:
